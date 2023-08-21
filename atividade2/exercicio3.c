08a9d7b14d1e8a6a5179fe692d05cbeded90a6ee
D.A <andressa.gabrielly.ofc@gmail.com>
2023-08-21 12:56:38 -0300

    Início

---
+++ atividade2/exercicio3.c
@@ -0,0 +1,19 @@
+//Exercício 3 - Verificar se um elemento existe no array. Escreva uma função que recebe um array de strings e uma string de busca, e retorna "1" se a string de busca existe no array, ou "0" caso não exista.//
+#include <stdio.h>
+#include <stdlib.h>
+
+void busca_string[char *array, char string]{
+    int i;
+    if()
+
+}
+
+int main (){
+    char string[1], array[3] = {"texto", "J", "EDA"};
+    int length = sizeof(array)/sizeof(array[0]);    
+    
+    printf("Vetor: ");
+    for (int i = 0; i < length; i++) {     
+        printf("%d ", array[i]);     
+    }
+}      
