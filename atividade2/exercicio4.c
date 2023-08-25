722f8c67cd14b73206843950ca8575b9d06436ab
D.A <andressa.gabrielly.ofc@gmail.com>
2023-08-25 10:03:51 -0300

    Solução Exercício4 (lista2)

--- atividade2/exercicio4.c
+++ atividade2/exercicio4.c
@@ -1,21 +1,32 @@
 //Contar a quantidade de ocorrências de um elemento no array. Escreva uma função que recebe um array de números inteiros e um número inteiro de busca, e retorna a quantidade de vezes que o número de busca aparece no array.//
 #include <stdio.h>
-#include <stdlib.h>
 
-void busca_string[int *array, int numbusca]{
-    for(int i=0; i)
-
+int count(const int array[], int arraySize, int busca) {
+    int count = 0;
+    for (int i = 0; i < arraySize; i++) {
+        if (array[i] == busca) {
+            count++;
+        }
+    }
+    return count;
 }
 
-int main (){
-    char string[1], array[3] = {"texto", "J", "EDA"};
+int main() {
+    int array[] = {5, 7, 9, 6, 7, 11, 6, 5};
+    int arraySize = sizeof(array) / sizeof(array[0]);
     int length = sizeof(array)/sizeof(array[0]);    
-
-    printf("Digite o numero inteiro que deseja buscar: ");
-    scanf()
     
     printf("Vetor: ");
     for (int i = 0; i < length; i++) {     
         printf("%d ", array[i]);     
-    }
-}      
+    }      
+    
+    int busca;
+    printf("Digite o valor que deseja buscar: ");
+    scanf("%d", &busca);
+    int occurrences = count(array, arraySize, busca);
+    
+    printf("O numero %d aparece %d vezes no array.\n", busca, occurrences);
+    
+    return 0;
+}
